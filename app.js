7132988467bf9c0044afaa79eff1712d53898dc2
kuroyuma <k.1993.yuma.0324@gmail.com>
2021-10-04 18:51:31 +0900

    [finish2]

--- app.js
+++ app.js
@@ -1,1 +1,57 @@
-alert('Hello World');
+let hello = 'Hello World';
+alert(hello);
+
+let init1 = 1;
+
+let init2 = -10;
+
+let float1 = 3.14;
+
+let str = 'JavaScriptを覚えよう';
+
+alert(float1);
+
+alert(4+3);
+
+alert(8-5)
+
+alert(2*6)
+
+alert(10/2)
+
+alert('Hello' + 'World')
+
+let str1 = 'Hello';
+let str2 = 'World!!';
+alert(str1 + str2);
+
+let orange = 100;
+let apple = 120;
+
+if(orange < apple){
+  alert('みかんの値段がりんごより安い');
+} else if(orange == apple){
+  alert('みかんとりんごが同じ値段');
+} else{
+  alert('みかんの値段がりんごより高い');
+}
+
+let max = 100;
+let num = 1;
+let count = 0;
+
+while(num < max){
+  num = num * 2;
+  count = count + 1;
+}
+
+alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+
+let i;
+let numb = 0;
+
+for(i = 1; i < 11; i++){
+  numb = numb + i;
+}
+
+alert('1から10まで足し算した結果は' + numb + 'です');
